f8196dedeb9322fe30db1995e4baf4ae1fa55527
sk15896 <146401512+sk15896@users.noreply.github.com>
2026-02-23 11:32:30 +0530

    added new file

---
+++ javascript file/sript.js
@@ -0,0 +1,28 @@
+function calculateGrade() {
+
+    let marks = document.getElementById("marks").value;
+    let grade;
+
+    if (marks >= 90) {
+        grade = "A+";
+    }
+    else if (marks >= 80) {
+        grade = "A";
+    }
+    else if (marks >= 70) {
+        grade = "B";
+    }
+    else if (marks >= 60) {
+        grade = "C";
+    }
+    else if (marks >= 50) {
+        grade = "D";
+    }
+    else {
+        grade = "Fail";
+    }
+
+    document.getElementById("result").innerHTML =
+    "Your Grade is: " + grade;
+
+}
